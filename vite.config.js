b0e5e3481fd5ad94165bd42f9f66f3fcc40f2b7b
leinadgp <leinadgp@gmail.com>
2025-07-28 10:49:08 -0300

    teste

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/projeto_dev_movies/', // 👈 nome exato do repositório
   plugins: [react()],
 })
